6d8ebedbe45681f7a938ec28f23f346f68fb4f3a
alessandraluiza <aleluizasg@gmail.com>
2024-08-02 21:42:57 -0300

    Curso Python Crie sua Primeira Aplicação

---
+++ hora_da_pratica.py
@@ -0,0 +1,13 @@
+print('Python na Escola de Programação da Alura')
+
+nome = input('Informe seu nome: ')
+idade = input('Informe sua idade: ')
+
+print(f'Meu nome é {nome} e tenho {idade} anos')
+
+print('A\nL\nU\nR\nA')
+
+
+pi = 3.14159
+arredondado = round(pi, 2)
+print(f'O valor arredondado de pi é: {arredondado}')
